340ad69fe62a45d4e16ba4729b8da3b9f0aa6135
HungKNguyen <75971367+HungKNguyen@users.noreply.github.com>
2023-01-25 14:37:39 -0600

    Hook up backend

--- src/minicomponents/WebAPIs.js
+++ src/minicomponents/WebAPIs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: 'http://146.190.174.56/api/',
     headers: {
         'Content-Type': 'application/json'
     }
